b31694e6636fef8f7be8c63f379344e7775c870e
Zhongmiao Li <mars.leezm@gmail.com>
2014-05-09 13:33:04 +0200

    Fixing bug of handoff

--- src/getminimum.erl
+++ src/getminimum.erl
@@ -1,48 +1,39 @@
 -module(getminimum).
--export([test/2, insert/4, insort/3, copyList/2]).
+-export([test/1, insert/3, insort/2, copyList/2]).
 
-test(List, Lazy) ->
-    %List = [1,0,2,3,4,8,9],
+test(List) ->
     {id, S1}=derflowdis:declare(),
     ets:new(haha, [set, named_table, public, {write_concurrency, true}]),
     ets:insert(haha,{count, 0}),
-    derflowdis:thread(getminimum,insort,[List, S1, Lazy]),
+    derflowdis:thread(getminimum,insort,[List, S1]),
     {V,_} = derflowdis:read(S1),
     io:format("Minimum ~w~n",[V]).
 
-
-insert(X, In, Out, Lazy) ->
+insert(X, In, Out) ->
     [{Id, C}] =ets:lookup(haha, count),
     ets:insert(haha,{Id,C+1}),
     io:format("Insert ~w ~n",[C+1]),
-    if Lazy == true -> 
     derflowdis:waitNeeded(Out);
-     true->
-	io:format("Not lazy")
-    end,
     case derflowdis:read(In) of 
 	{nil,_} -> %io:format("Reading end ~w~n",[X]), 
 		   {id,Next} = derflowdis:bind(Out,X),
 		   derflowdis:bind(Next, nil);
 	{V, SNext} ->
-		%io:format("The head is ~w, to insert is ~w ~n",[V, X]),
 		if X<V ->
 			{id, Next} = derflowdis:bind(Out,X), 
 			derflowdis:bind(Next, In);
-			%copyList(NextKey, In);
 		 true -> 
 			{id,Next} = derflowdis:bind(Out,V),
-			insert(X, SNext, Next,Lazy)
+			insert(X, SNext, Next)
 		 end
     end.
 
-insort(List, S, Lazy) ->
+insort(List, S) ->
     case List of [H|T] ->
 	   {id,OutS} = derflowdis:declare(),
-	   insort(T, OutS, Lazy),
-	   derflowdis:thread(getminimum, insert, [H, OutS, S, Lazy]);
+	   insort(T, OutS),
+	   derflowdis:thread(getminimum, insert, [H, OutS, S]);
 	[] ->
-	   %io:format("Binding to nothing~n"),
 	   derflowdis:bind(S,nil)
     end.
 

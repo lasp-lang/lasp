8e25ecaf4fa42cefbe8c6383e286aa002e704cae
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-08-22 23:08:56 -0700

    Update test.
    Update test to use the remote code loading harness.

--- riak_test/derflow_bind_test.erl
+++ riak_test/derflow_bind_test.erl
@@ -3,6 +3,8 @@
 -module(derflow_bind_test).
 -author("Christopher Meiklejohn <cmeiklejohn@basho.com>").
 
+-export([test/0]).
+
 -ifdef(TEST).
 
 -export([confirm/0]).
@@ -14,25 +16,34 @@
 confirm() ->
     [Nodes] = rt:build_clusters([1]),
     lager:info("Nodes: ~p", [Nodes]),
-
     Node = hd(Nodes),
 
-    {ok, Id} = derflow_test_helpers:declare(Node),
+    lager:info("Remotely loading code on node ~p", [Node]),
+    ok = derflow_test_helpers:load(Nodes),
+    lager:info("Remote code loading complete."),
 
-    ok = derflow_test_helpers:bind(Node, Id, 1),
-    lager:info("Successful bind."),
+    lager:info("Remotely executing the test."),
+    ?assertEqual({ok, 1, 1}, rpc:call(Node, ?MODULE, test, [])),
 
-    {ok, Value} = derflow_test_helpers:read(Node, Id),
-    lager:info("Value: ~p", [Value]),
-    ?assertEqual(1, Value),
-
-    error = derflow_test_helpers:bind(Node, Id, 2),
-    lager:info("Unsuccessful bind."),
-
-    {ok, Value} = derflow_test_helpers:read(Node, Id),
-    lager:info("Value: ~p", [Value]),
-    ?assertEqual(1, Value),
+    lager:info("Done!"),
 
     pass.
 
 -endif.
+
+test() ->
+    {ok, Id} = derflow:declare(),
+
+    ok = derflow:bind(Id, 1),
+    lager:info("Successful bind."),
+
+    {ok, Value1} = derflow:read(Id),
+    lager:info("Value1: ~p", [Value1]),
+
+    error = derflow:bind(Id, 2),
+    lager:info("Unsuccessful bind."),
+
+    {ok, Value2} = derflow:read(Id),
+    lager:info("Value2: ~p", [Value2]),
+
+    {ok, Value1, Value2}.

1797f04e0119c8d018916ba8412ed9544cedb969
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-11-01 12:04:07 -0700

    Remove riak_kv related files.
    Remove riak_kv related files, which are currently unused.

--- src/derflow_riak_kv.erl
+++
@@ -1,8 +0,0 @@
--module(derflow_riak_kv).
--include("derflow.hrl").
-
--export([produce/3]).
-
-produce(Object, Reason, Partition) ->
-    lager:info("Received object: ~p reason: ~p, partition: ~p",
-               [Object, Reason, Partition]).
--- src/derflow_riak_kv_integration.erl
+++
@@ -1,8 +0,0 @@
--module(derflow_riak_kv_integration).
--author("Christopher Meiklejohn <cmeiklejohn@basho.com>").
-
--export([postcommit_log_object/1]).
-
-%% @doc Trigger a log message when postcommit hook is triggered.
-postcommit_log_object(Object) ->
-    lager:info("Postcommit hook triggered for ~p", [Object]).

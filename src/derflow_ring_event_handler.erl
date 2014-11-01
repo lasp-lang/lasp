29202e946eb4f1da648b89cce44796305504180e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-11-01 12:00:26 -0700

    Add copyright.
    Add copyright information to the derflow source.

--- src/derflow_ring_event_handler.erl
+++ src/derflow_ring_event_handler.erl
@@ -1,3 +1,7 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
 %% except in compliance with the License.  You may obtain
@@ -11,8 +15,8 @@
 %% KIND, either express or implied.  See the License for the
 %% specific language governing permissions and limitations
 %% under the License.
-
-%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% -------------------------------------------------------------------
 
 -module(derflow_ring_event_handler).
 -behaviour(gen_event).

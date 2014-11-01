58f20b62845e363972292ac1c864b672143d9ebf
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-11-01 12:02:13 -0700

    Add copyright.
    Add copyright and author information.

--- src/riak_dt_sum.erl
+++ src/riak_dt_sum.erl
@@ -1,3 +1,23 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
 %% -*- coding: utf-8 -*-
 %%
 %% @doc A Sum C-CRDT: for counting things!
@@ -11,6 +31,8 @@
 %% @end
 
 -module(riak_dt_sum).
+-author("Christopher Meiklejohn <cmeiklejohn@basho.com>").
+
 -behaviour(riak_dt).
 
 -export([new/0, new/2, value/1, value/2,

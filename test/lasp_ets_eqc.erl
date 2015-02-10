38cfdad360e88b0e5aa4c1d07c70130d8ba16c9f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-02-09 20:17:36 -0500

    Rewrite EQC.
    Rewrite EQC to be cleaner, and easier to understand.  Fix test
    distribution by deriving the read thresholds from the currently bound
    values.

--- test/lasp_ets_eqc.erl
+++ test/lasp_ets_eqc.erl
@@ -1,3 +1,26 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc EQC test for the Lasp ETS backend.
+%%
+
 -module(lasp_ets_eqc).
 -author("Christopher Meiklejohn <cmeiklejohn@basho.com>").
 
@@ -15,13 +38,6 @@
 -record(state, {store}).
 -record(variable, {type, value}).
 
--define(LATTICES, [lasp_ivar,
-                   riak_dt_gset,
-                   riak_dt_gcounter,
-                   riak_dt_orset,
-                   riak_dt_map,
-                   riak_dt_orswot]).
-
 -define(ETS, lasp_ets_eqc).
 
 -define(QC_OUT(P),
@@ -32,6 +48,133 @@
 lasp_ets_sequential_test_() ->
     {timeout, 60,
      ?_assert(eqc:quickcheck(?QC_OUT(?MODULE:prop_sequential())))}.
+
+%% Initialize state.
+
+initial_state() ->
+    #state{store=dict:new()}.
+
+%% Declaring new variables.
+
+declare(Type) ->
+    {ok, Id} = lasp_ets:declare(Type, ?ETS),
+    Id.
+
+declare_args(_S) ->
+    [elements([lasp_ivar,
+               riak_dt_gset,
+               riak_dt_orset])].
+
+declare_next(#state{store=Store0}=S, Res, [Type]) ->
+    Store = dict:store(Res,
+                       #variable{type=Type, value=Type:new()}, Store0),
+    S#state{store=Store}.
+
+%% Bind variables.
+
+bind(Id, Value) ->
+    lasp_ets:bind(Id, Value, ?ETS).
+
+bind_pre(S) ->
+    has_variables(S).
+
+bind_args(#state{store=Store}) ->
+    Variables = dict:fetch_keys(Store),
+    ?LET(Variable, elements(Variables),
+        begin
+            Value = value(Variable, Store),
+            [Variable, Value]
+        end).
+
+bind_post(S, [Id, NewValue], Res) ->
+    case bind_ok(S, [Id, NewValue]) of
+        true ->
+            ok(Res);
+        false ->
+            is_error(Res)
+    end.
+
+bind_next(#state{store=Store0}=S, _Res, [Id, NewValue]) ->
+    Store = case bind_ok(S, [Id, NewValue]) of
+        true ->
+            case dict:find(Id, Store0) of
+                {ok, #variable{type=Type, value=Value}=Variable} ->
+                    try
+                        Merged = Type:merge(Value, NewValue),
+                        case lasp_lattice:is_inflation(Type, Value, Merged) of
+                            true ->
+                                dict:store(Id,
+                                           Variable#variable{value=NewValue},
+                                           Store0);
+                            false ->
+                                Store0
+                        end
+                    catch
+                        _:_ ->
+                            Store0
+                    end
+            end;
+        false ->
+            Store0
+    end,
+    S#state{store=Store}.
+
+bind_ok(#state{store=Store0}, [Id, NewValue]) ->
+    case dict:find(Id, Store0) of
+        {ok, #variable{type=Type, value=Value}} ->
+            try
+                Merged = Type:merge(Value, NewValue),
+                lasp_lattice:is_inflation(Type, Value, Merged)
+            catch
+                _:_ ->
+                    %% In event of a merge which fails, we still return
+                    %% ok, but do not change the value.  This might need
+                    %% to change.
+                    %%
+                    true
+            end;
+        _ ->
+            false
+    end.
+
+%% Read variables.
+
+read(Id, Threshold) ->
+    lasp_ets:read(Id, Threshold, ?ETS).
+
+read_pre(S) ->
+    has_variables(S).
+
+read_pre(#state{store=Store}, [Id, _Threshold]) ->
+    case dict:find(Id, Store) of
+        error ->
+            %% Not declared.
+            false;
+        {ok, #variable{value=undefined}} ->
+            %% Not bound.
+            false;
+        {ok, #variable{value=_Value, type=_Type}} ->
+            true
+    end.
+
+read_args(#state{store=Store}) ->
+    Variables = dict:fetch_keys(Store),
+    ?LET(Variable, elements(Variables),
+                    begin
+                        Threshold = threshold(Variable, Store),
+                        [Variable, Threshold]
+                    end).
+
+read_post(S, [Id, Threshold], Res) ->
+    case read_ok(S, [Id, Threshold]) of
+        true ->
+            ok(Res);
+        false ->
+            is_error(Res)
+    end.
+
+read_ok(_S, [_Id, _Threshold]) ->
+    true.
 
 %% Generators.
 
@@ -52,135 +195,39 @@ value(Variable, Store) ->
 
 threshold(Variable, Store) ->
     case dict:find(Variable, Store) of
-        {ok, #variable{type=Type}} ->
-            ?LET({Object, Update},
-                 {Type:new(), Type:gen_op()},
-                  begin
-                    case Type:update(Update, undefined, Object) of
-                        {ok, X} ->
-                            X;
+        {ok, #variable{type=Type, value=Value0}} ->
+            case Type of
+                lasp_ivar ->
+                    case Value0 of
+                        undefined ->
+                            undefined;
                         _ ->
-                            Object
+                            {strict, undefined}
+                    end;
+                riak_dt_gset ->
+                    Value = Type:value(Value0),
+                    Length = length(Value),
+                    case Length of
+                        0 ->
+                            [];
+                        _ ->
+                            Random = random:uniform(Length),
+                            lists:sublist(Value, Random, Length)
+                    end;
+                riak_dt_orset ->
+                    %% @TODO: should toggle the boolean flag on some
+                    %% elements, if possible.
+                    %%
+                    Length = length(Value0),
+                    case Length of
+                        0 ->
+                            [];
+                        _ ->
+                            Random = random:uniform(Length),
+                            lists:sublist(Value0, Random, Length)
                     end
-                  end)
-    end.
-
-%% Initialize state
-
-initial_state() ->
-    #state{store=dict:new()}.
-
-%% Declaring new variables.
-
-declare(Type) ->
-    {ok, Id} = lasp_ets:declare(Type, ?ETS),
-    Id.
-
-declare_args(_S) ->
-    [elements(?LATTICES)].
-
-declare_next(#state{store=Store0}=S, V, [Type]) ->
-    Store = dict:store(V, #variable{type=Type}, Store0),
-    S#state{store=Store}.
-
-%% Binding variables.
-%%
-%% If a bind failed, that's only allowed if the variable is already
-%% bound or undefined.
-
-bind(Id, Value) ->
-    lasp_ets:bind(Id, Value, ?ETS).
-
-bind_pre(S) ->
-    has_variables(S).
-
-bind_args(#state{store=Store}) ->
-    Variables = dict:fetch_keys(Store),
-    ?LET(Variable, elements(Variables),
-        begin
-            Value = value(Variable, Store),
-            [Variable, Value]
-        end).
-
-bind_next(#state{store=Store0}=S, _V, [Id, NewValue]) ->
-    %% Only update the record, if it's in inflation or has never been
-    %% updated before.
-    Store = case dict:find(Id, Store0) of
-        {ok, #variable{type=Type, value=Value}=Variable} ->
-            try
-                Merged = case Value of
-                    undefined ->
-                        NewValue;
-                    _ ->
-                        Type:merge(Value, NewValue)
-                end,
-                case lasp_lattice:is_inflation(Type, Value, Merged) of
-                    true ->
-                        dict:store(Id, Variable#variable{value=Merged}, Store0);
-                    false ->
-                        Store0
-                end
-            catch
-                _:_ ->
-                    Store0
             end
-    end,
-    S#state{store=Store}.
-
-bind_post(#state{store=Store}, [Id, V], error) ->
-    case dict:find(Id, Store) of
-        {ok, #variable{type=_Type, value=undefined}} ->
-            false;
-        {ok, #variable{type=Type, value=Value}} ->
-            try
-                Merged = Type:merge(V, Value),
-                case lasp_lattice:is_inflation(Type, Value, Merged) of
-                    true ->
-                        false;
-                    false ->
-                        true
-                end
-            catch
-                _:_ ->
-                    true
-            end
-    end;
-bind_post(_, _, _) ->
-    true.
-
-%% Read variables.
-
-read(Id, Threshold) ->
-    lasp_ets:read(Id, Threshold, ?ETS).
-
-read_pre(S) ->
-    has_variables(S).
-
-read_pre(S, [Id, Threshold]) ->
-    is_read_valid(S, Id, Threshold).
-
-read_args(#state{store=Store}) ->
-    Variables = dict:fetch_keys(Store),
-    ?LET(Variable, elements(Variables),
-                    begin
-                        Threshold = threshold(Variable, Store),
-                        [Variable, Threshold]
-                    end).
-
-read_post(#state{store=Store}, [Id, _Threshold], {ok, {_, V, _}}) ->
-    case dict:find(Id, Store) of
-        {ok, #variable{value=Value}} ->
-            Value == V;
-        _ ->
-            false
     end.
-
-%% Weights.
-
-weight(_, declare) -> 1;
-weight(_, bind) -> 1;
-weight(_, read) -> 2;
-weight(_, _) -> 3.
 
 %% Predicates.
 
@@ -188,22 +235,29 @@ has_variables(#state{store=Store}) ->
     Variables = dict:fetch_keys(Store),
     length(Variables) > 0.
 
-is_read_valid(#state{store=Store}, Id, Threshold) ->
-    case dict:find(Id, Store) of
-        error ->
-            %% Not declared.
-            false;
-        {ok, #variable{value=undefined}} ->
-            %% Not bound.
-            false;
-        {ok, #variable{value=Value, type=Type}} ->
-            case lasp_lattice:threshold_met(Type, Value, Threshold) of
-                true ->
-                    true;
-                false ->
-                    false
-            end
-    end.
+ok({ok, _}) ->
+    true;
+ok(_) ->
+    false.
+
+is_error(error) ->
+    true;
+is_error(Other) ->
+    {expected_error, Other}.
+
+%% Weights.
+
+weight(_, declare) ->
+    1;
+
+weight(_, bind) ->
+    1;
+
+weight(_, read) ->
+    2;
+
+weight(_, _) ->
+    3.
 
 %% Properties for the state machine.
 

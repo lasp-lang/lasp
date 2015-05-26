1c40bb6fc7c8e27828d71818d39a62bf6a48a1da
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-05-26 15:47:33 -0400

    Extract out gen_flow.
    Extract out the gen_flow behaviour from lasp_process.  Provide a way to
    use a generic abstraction style with behaviours for definiting generic
    dataflow programming "processing elements."

---
+++ src/gen_flow.erl
@@ -0,0 +1,120 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(gen_flow).
+-author('Christopher Meiklejohn <christopher.meiklejohn@gmail.com>').
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+%% API
+-export([start_link/2]).
+
+%% Callbacks
+-export([start/2]).
+
+%%%===================================================================
+%%% Behaviour
+%%%===================================================================
+
+-type state() :: term().
+
+-callback init(list(term())) -> {ok, state()}.
+-callback read(state()) -> {ok, state()}.
+-callback process(list(term()), state()) -> {ok, state()}.
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start_link(Module, Args) ->
+    Pid = spawn(?MODULE, start, [Module, Args]),
+    {ok, Pid}.
+
+%%%===================================================================
+%%% Callbacks
+%%%===================================================================
+
+%% @doc TODO
+start(Module, Args) ->
+    %% Initialize state.
+    {ok, State} = Module:init(Args),
+    loop(Module, State, orddict:new()).
+
+%% @doc TODO
+loop(Module, State0, Cache0) ->
+    %% Get self.
+    Self = self(),
+
+    %% Gather the read functions.
+    {ok, ReadFuns, ReadState} = Module:read(State0),
+
+    %% Initialize bottom values in orddict.
+    DefaultedCache = lists:foldl(fun(X, C) ->
+                case orddict:find(X, C) of
+                    error ->
+                        orddict:store(X, undefined, C);
+                    {ok, _} ->
+                        C
+                end
+        end, Cache0, lists:seq(1, length(ReadFuns))),
+
+    %% For each readfun, spawn a linked process to request values.
+    lists:foreach(fun(X) ->
+            ReadFun = lists:nth(X, ReadFuns),
+            spawn_link(fun() ->
+                            Value = ReadFun(),
+                            Self ! {ok, X, Value}
+                    end)
+        end, lists:seq(1, length(ReadFuns))),
+
+    %% Wait for responses.
+    receive
+        {ok, X, V} ->
+            %% Update cache.
+            Cache = orddict:store(X, V, DefaultedCache),
+
+            %% Get current values from cache.
+            RealizedCache = [Value || {_, Value}
+                                      <- orddict:to_list(Cache)],
+
+            %% Call process function.
+            {ok, State} = Module:process(RealizedCache, ReadState),
+
+            %% Wait.
+            loop(Module, State, Cache)
+    end.
+
+-ifdef(TEST).
+
+gen_flow_test() ->
+    {ok, _Pid} = gen_flow:start_link(gen_flow_example, [self()]),
+
+    Response = receive
+        ok ->
+            ok;
+        {ok, X} ->
+            X
+    end,
+
+    ?assertEqual([1,2], Response).
+
+-endif.
---
+++ src/gen_flow_example.erl
@@ -0,0 +1,67 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(gen_flow_example).
+-author('Christopher Meiklejohn <christopher.meiklejohn@gmail.com>').
+
+-behaviour(gen_flow).
+
+%% API
+-export([start_link/0]).
+
+%% Callbacks
+-export([init/1, read/1, process/2]).
+
+%% Records
+-record(state, {source}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start_link() ->
+    {ok, Pid} = gen_flow:start_link(?MODULE),
+    {ok, Pid}.
+
+%%%===================================================================
+%%% Callbacks
+%%%===================================================================
+
+%% @doc Initialize state.
+init([Source]) ->
+    {ok, #state{source=Source}}.
+
+%% @doc Return list of read functions.
+read(State) ->
+    ReadFuns = [fun() -> 1 end, fun() -> 2 end],
+    {ok, ReadFuns, State}.
+
+%% @doc Computation to execute when inputs change.
+process(Args, #state{source=Source}=State) ->
+    case Args of
+        [undefined, _] ->
+            ok;
+        [_, undefined] ->
+            ok;
+        [_, _] ->
+            Source ! {ok, Args},
+            ok
+    end,
+    {ok, State}.

8ec94af61f2f9cf1789cdf0375bd367bbc36633a
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-06-06 22:43:13 -0400

    Speed up the test by using one node.

--- riak_test/lasp_bind_test.erl
+++ riak_test/lasp_bind_test.erl
@@ -34,7 +34,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 confirm() ->
-    [Nodes] = rt:build_clusters([2]),
+    [Nodes] = rt:build_clusters([1]),
     lager:info("Nodes: ~p", [Nodes]),
     Node = hd(Nodes),
 

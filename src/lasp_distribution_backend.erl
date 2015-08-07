aad26a542c370f29cd465358612e8781096990e7
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-08-07 16:12:38 -0400

    Remove invalid @doc.

--- src/lasp_distribution_backend.erl
+++ src/lasp_distribution_backend.erl
@@ -23,105 +23,105 @@
 
 -include("lasp.hrl").
 
-%% @doc Declare a new dataflow variable of a given type.
+%% Declare a new dataflow variable of a given type.
 %%
-%%      Valid values for `Type' are any of lattices supporting the
-%%      `riak_dt' behavior.  Type is declared with the provided `Id'.
+%% Valid values for `Type' are any of lattices supporting the
+%% `riak_dt' behavior.  Type is declared with the provided `Id'.
 %%
 -callback declare(id(), type()) -> {ok, id()} | error().
 
-%% @doc Update a dataflow variable.
+%% Update a dataflow variable.
 %%
-%%      Read the given `Id' and update it given the provided
-%%      `Operation', which should be valid for the type of CRDT stored
-%%      at the given `Id'.
+%% Read the given `Id' and update it given the provided
+%% `Operation', which should be valid for the type of CRDT stored
+%% at the given `Id'.
 %%
 -callback update(id(), operation(), actor()) -> {ok, {value(), id()}} | error().
 
-%% @doc Bind a dataflow variable to a value.
+%% Bind a dataflow variable to a value.
 %%
-%%      The provided `Id' is identifier of a previously declared (see:
-%%      {@link declare/0}) dataflow variable.  The `Value' provided is
-%%      the value to bind.
+%% The provided `Id' is identifier of a previously declared (see:
+%% {@link declare/0}) dataflow variable.  The `Value' provided is
+%% the value to bind.
 %%
 -callback bind(id(), value()) -> {ok, var()} | error().
 
-%% @doc Bind a dataflow variable to another dataflow variable.
+%% Bind a dataflow variable to another dataflow variable.
 %%
-%%      The provided `Id' is identifier of a previously declared (see:
-%%      {@link declare/0}) dataflow variable.  The `Value' provided is
-%%      the value to bind.
+%% The provided `Id' is identifier of a previously declared (see:
+%% {@link declare/0}) dataflow variable.  The `Value' provided is
+%% the value to bind.
 %%
 -callback bind_to(id(), id()) -> {ok, id()} | error().
 
-%% @doc Blocking monotonic read operation for a given dataflow variable.
+%% Blocking monotonic read operation for a given dataflow variable.
 %%
-%%      Block until the variable identified by `Id' has been bound, and
-%%      is monotonically greater (as defined by the lattice) then the
-%%      provided `Threshold' value.
+%% Block until the variable identified by `Id' has been bound, and
+%% is monotonically greater (as defined by the lattice) then the
+%% provided `Threshold' value.
 %%
 -callback read(id(), threshold()) -> {ok, var()} | error().
 
-%% @doc Blocking monotonic read operation for a list of given dataflow
-%%      variables.
+%% Blocking monotonic read operation for a list of given dataflow
+%% variables.
 %%
 -callback read_any([{id(), threshold()}]) -> {ok, var()} | error().
 
-%% @doc Compute the cartesian product of two sets.
+%% Compute the cartesian product of two sets.
 %%
-%%      Computes the cartestian product of two sets and bind the result
-%%      to a third.
+%% Computes the cartestian product of two sets and bind the result
+%% to a third.
 %%
 -callback product(id(), id(), id()) -> ok | error().
 
-%% @doc Compute the union of two sets.
+%% Compute the union of two sets.
 %%
-%%      Computes the union of two sets and bind the result
-%%      to a third.
+%% Computes the union of two sets and bind the result
+%% to a third.
 %%
 -callback union(id(), id(), id()) -> ok | error().
 
-%% @doc Compute the intersection of two sets.
+%% Compute the intersection of two sets.
 %%
-%%      Computes the intersection of two sets and bind the result
-%%      to a third.
+%% Computes the intersection of two sets and bind the result
+%% to a third.
 %%
 -callback intersection(id(), id(), id()) -> ok | error().
 
-%% @doc Map values from one lattice into another.
+%% Map values from one lattice into another.
 %%
-%%      Applies the given `Function' as a map over the items in `Id',
-%%      placing the result in `AccId', both of which need to be declared
-%%      variables.
+%% Applies the given `Function' as a map over the items in `Id',
+%% placing the result in `AccId', both of which need to be declared
+%% variables.
 %%
 -callback map(id(), function(), id()) -> ok | error().
 
-%% @doc Fold values from one lattice into another.
+%% Fold values from one lattice into another.
 %%
-%%      Applies the given `Function' as a fold over the items in `Id',
-%%      placing the result in `AccId', both of which need to be declared
-%%      variables.
+%% Applies the given `Function' as a fold over the items in `Id',
+%% placing the result in `AccId', both of which need to be declared
+%% variables.
 %%
 -callback fold(id(), function(), id()) -> ok | error().
 
-%% @doc Filter values from one lattice into another.
+%% Filter values from one lattice into another.
 %%
-%%      Applies the given `Function' as a filter over the items in `Id',
-%%      placing the result in `AccId', both of which need to be declared
-%%      variables.
+%% Applies the given `Function' as a filter over the items in `Id',
+%% placing the result in `AccId', both of which need to be declared
+%% variables.
 %%
 -callback filter(id(), function(), id()) -> ok | error().
 
-%% @doc Spawn a function.
+%% Spawn a function.
 %%
-%%      Spawn a process executing `Module:Function(Args)'.
+%% Spawn a process executing `Module:Function(Args)'.
 %%
 -callback thread(module(), func(), args()) -> ok | error().
 
-%% @doc Pause execution until value requested with given threshold.
+%% Pause execution until value requested with given threshold.
 %%
-%%      Pause execution of calling thread until a read operation is
-%%      issued for the given `Id'.  Used to introduce laziness into a
-%%      computation.
+%% Pause execution of calling thread until a read operation is
+%% issued for the given `Id'.  Used to introduce laziness into a
+%% computation.
 %%
 -callback wait_needed(id(), threshold()) -> ok | error().

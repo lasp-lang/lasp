124982110527547c7f053166f4f72ddcdf02fe54
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-09-07 19:09:58 -0700

    Update console commands.

--- src/lasp_console.erl
+++ src/lasp_console.erl
@@ -20,18 +20,12 @@
 
 -module(lasp_console).
 
--export([staged_join/1,
-         down/1,
-         ringready/1]).
--ignore_xref([join/1,
-              leave/1,
-              remove/1,
-              ringready/1]).
+-export([join/1]).
 
-staged_join([NodeStr]) ->
+join([NodeStr]) ->
     Node = list_to_atom(NodeStr),
-    join(NodeStr, fun riak_core:staged_join/1,
-         "Success: staged join request for ~p to ~p~n", [node(), Node]).
+    join(NodeStr, fun lasp_peer_service:join/1,
+         "Success: join request for ~p to ~p~n", [node(), Node]).
 
 join(NodeStr, JoinFn, SuccessFmt, SuccessArgs) ->
     try
@@ -39,24 +33,6 @@ join(NodeStr, JoinFn, SuccessFmt, SuccessArgs) ->
             ok ->
                 io:format(SuccessFmt, SuccessArgs),
                 ok;
-            {error, not_reachable} ->
-                io:format("Node ~s is not reachable!~n", [NodeStr]),
-                error;
-            {error, different_ring_sizes} ->
-                io:format("Failed: ~s has a different ring_creation_size~n",
-                          [NodeStr]),
-                error;
-            {error, unable_to_get_join_ring} ->
-                io:format("Failed: Unable to get ring from ~s~n", [NodeStr]),
-                error;
-            {error, not_single_node} ->
-                io:format("Failed: This node is already a member of a "
-                          "cluster~n"),
-                error;
-            {error, self_join} ->
-                io:format("Failed: This node cannot join itself in a "
-                          "cluster~n"),
-                error;
             {error, _} ->
                 io:format("Join failed. Try again in a few moments.~n", []),
                 error
@@ -65,50 +41,5 @@ join(NodeStr, JoinFn, SuccessFmt, SuccessArgs) ->
         Exception:Reason ->
             lager:error("Join failed ~p:~p", [Exception, Reason]),
             io:format("Join failed, see log for details~n"),
-            error
-    end.
-
-
-down([Node]) ->
-    try
-        case riak_core:down(list_to_atom(Node)) of
-            ok ->
-                io:format("Success: ~p marked as down~n", [Node]),
-                ok;
-            {error, is_up} ->
-                io:format("Failed: ~s is up~n", [Node]),
-                error;
-            {error, not_member} ->
-                io:format("Failed: ~p is not a member of the cluster.~n",
-                          [Node]),
-                error;
-            {error, only_member} ->
-                io:format("Failed: ~p is the only member.~n", [Node]),
-                error
-        end
-    catch
-        Exception:Reason ->
-            lager:error("Down failed ~p:~p", [Exception, Reason]),
-            io:format("Down failed, see log for details~n"),
-            error
-    end.
-
-ringready([]) ->
-    try
-        case riak_core_status:ringready() of
-            {ok, Nodes} ->
-                io:format("TRUE All nodes agree on the ring ~p\n", [Nodes]);
-            {error, {different_owners, N1, N2}} ->
-                io:format("FALSE Node ~p and ~p list different partition owners\n", [N1, N2]),
-                error;
-            {error, {nodes_down, Down}} ->
-                io:format("FALSE ~p down.  All nodes need to be up to check.\n", [Down]),
-                error
-        end
-    catch
-        Exception:Reason ->
-            lager:error("Ringready failed ~p:~p", [Exception,
-                    Reason]),
-            io:format("Ringready failed, see log for details~n"),
             error
     end.

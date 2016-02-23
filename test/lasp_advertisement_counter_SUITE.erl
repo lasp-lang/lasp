7ad26023c162b3192748d5be613d2dabac5e72a2
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-02-23 13:46:47 -0800

    Instrument test.

--- test/lasp_advertisement_counter_SUITE.erl
+++ test/lasp_advertisement_counter_SUITE.erl
@@ -68,7 +68,7 @@ init_per_testcase(Case, Config) ->
                   end, Rest),
 
     %% Wait until convergence.
-    lasp_test_utils:wait_until_joined(Nodes, Nodes),
+    ok = lasp_test_utils:wait_until_joined(Nodes, Nodes),
     ct:pal("Cluster converged."),
 
     {ok, _} = ct_cover:add_nodes(Nodes),
@@ -90,18 +90,21 @@ all() ->
 %% ===================================================================
 
 advertisement_counter_orset_gcounter_10000_100_10_test(Config) ->
+    ct:pal("Executing advertisement_counter_orset_gcounter_10000_100_10_test..."),
     [Node1 | _Nodes] = proplists:get_value(nodes, Config),
     {ok, _} = rpc:call(Node1, lasp_advertisement_counter, run,
                        [[lasp_orset, lasp_gcounter, 10000, 100, 10]]),
     ok.
 
 advertisement_counter_orset_gcounter_10000_500_10_test(Config) ->
+    ct:pal("Executing advertisement_counter_orset_gcounter_10000_500_10_test..."),
     [Node1 | _Nodes] = proplists:get_value(nodes, Config),
     {ok, _} = rpc:call(Node1, lasp_advertisement_counter, run,
                        [[lasp_orset, lasp_gcounter, 10000, 500, 10]]),
     ok.
 
 advertisement_counter_orset_gcounter_10000_1000_10_test(Config) ->
+    ct:pal("Executing advertisement_counter_orset_gcounter_10000_1000_10_test..."),
     [Node1 | _Nodes] = proplists:get_value(nodes, Config),
     {ok, _} = rpc:call(Node1, lasp_advertisement_counter, run,
                        [[lasp_orset, lasp_gcounter, 10000, 1000, 10]]),

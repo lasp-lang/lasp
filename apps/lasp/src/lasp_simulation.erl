a8e88a733b1e5a509df427452fc7856c805537ae
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-02-26 17:42:45 -0800

    Instrument the simulation with light logging.

--- apps/lasp/src/lasp_simulation.erl
+++ apps/lasp/src/lasp_simulation.erl
@@ -63,23 +63,29 @@
 %%         you like some help?
 %%
 run(Module, Args) ->
+    lager:info("Initializing simulation!"),
     {ok, State} = Module:init(Args),
 
     %% Unfortunately, we have to wait for the cluster to stabilize, else
     %% some of the clients running at other node will get not_found
     %% operations.
+    lager:info("Waiting for cluster to stabilize..."),
     timer:sleep(2000),
 
     %% Launch client processes.
+    lager:info("Launching clients!"),
     {ok, State1} = Module:clients(State),
 
     %% Initialize simulation.
+    lager:info("Running simulation!"),
     {ok, State2} = Module:simulate(State1),
 
     %% Wait until we receive num events.
+    lager:info("Waiting for event generation to complete!"),
     {ok, State3} = Module:wait(State2),
 
     %% Terminate all clients.
+    lager:info("Terminating clients!"),
     {ok, State4} = Module:terminate(State3),
 
     %% Finish and summarize.

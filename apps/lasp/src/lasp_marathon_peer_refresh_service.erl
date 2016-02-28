ad85bc1b8c9e19b089c1b78c6bf598508a95a10e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-02-27 23:47:24 -0800

    Fix stupid typo.

--- apps/lasp/src/lasp_marathon_peer_refresh_service.erl
+++ apps/lasp/src/lasp_marathon_peer_refresh_service.erl
@@ -186,7 +186,7 @@ connect(Node) ->
 %% @private
 request() ->
     IP = os:getenv("IP", "127.0.0.1"),
-    Url = "http://" + IP + ":8080/v2/apps/lasp?embed=app.taskStats",
+    Url = "http://" ++ IP ++ ":8080/v2/apps/lasp?embed=app.taskStats",
     case httpc:request(get, {Url, []}, [], [{body_format, binary}]) of
         {ok, {{_, 200, _}, _, Body}} ->
             {ok, jsx:decode(Body, [return_maps])};

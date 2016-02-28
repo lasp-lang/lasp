de90cb6bdc0d24cdaec5a54c6f2df2abccc7a68c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-02-27 23:45:55 -0800

    Use IP environment.

--- apps/lasp/src/lasp_marathon_peer_refresh_service.erl
+++ apps/lasp/src/lasp_marathon_peer_refresh_service.erl
@@ -185,7 +185,8 @@ connect(Node) ->
 
 %% @private
 request() ->
-    Url = "http://10.141.141.10:8080/v2/apps/lasp?embed=app.taskStats",
+    IP = os:getenv("IP", "127.0.0.1"),
+    Url = "http://" + IP + ":8080/v2/apps/lasp?embed=app.taskStats",
     case httpc:request(get, {Url, []}, [], [{body_format, binary}]) of
         {ok, {{_, 200, _}, _, Body}} ->
             {ok, jsx:decode(Body, [return_maps])};

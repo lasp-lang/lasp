6341785fd0a8848ba2b1ccd9252c0ff6776b2c1c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-04-28 18:58:42 +0200

    Convert server to a gen_server.

--- src/lasp_peer_protocol_server.erl
+++ src/lasp_peer_protocol_server.erl
@@ -24,19 +24,39 @@
 -author("Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>").
 
 -behaviour(ranch_protocol).
+-behaviour(gen_server).
 
 %% ranch_protocol callbacks.
 -export([start_link/4,
          init/4]).
 
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
 -define(TIMEOUT, 10000).
 
-start_link(ListenerPid, Socket, Transport, Options) ->
-    Pid = spawn_link(?MODULE, init, [ListenerPid, Socket, Transport, Options]),
-    {ok, Pid}.
+-record(state, {socket, transport}).
 
-%% @doc Initialize connection.
+%%%===================================================================
+%%% ranch_protocol callbacks
+%%%===================================================================
+
+%% @private
+start_link(ListenerPid, Socket, Transport, Options) ->
+    proc_lib:start_link(?MODULE,
+                        init,
+                        [ListenerPid, Socket, Transport, Options]).
+
+%% @private
 init(ListenerPid, Socket, Transport, _Options) ->
+    %% Acknowledge process initialization.
+    ok = proc_lib:init_ack({ok, self()}),
+
     %% Acknowledge the connection.
     ok = ranch:accept_ack(ListenerPid),
 
@@ -49,38 +69,80 @@ init(ListenerPid, Socket, Transport, _Options) ->
     %% Generate the welcome message, encode it and transmit the message.
     send_message(Socket, Transport, {hello, node()}),
 
-    %% Wait for response.
-    Message = receive_message(Socket, Transport),
-    case Message of
-        {hello, Node} ->
+    %% Enter the gen_server loop.
+    gen_server:enter_loop(?MODULE,
+                          [],
+                          #state{socket=Socket, transport=Transport}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%% @private
+init([ListenerPid, Socket, Transport, Options]) ->
+    init(ListenerPid, Socket, Transport, Options).
+
+%% @private
+-spec handle_call(term(), {pid(), term()}, #state{}) ->
+    {reply, term(), #state{}}.
+
+%% @private
+handle_call(Msg, _From, State) ->
+    lager:warning("Unhandled messages: ~p", [Msg]),
+    {reply, ok, State}.
+
+-spec handle_cast(term(), #state{}) -> {noreply, #state{}}.
+
+%% @private
+handle_cast(Msg, State) ->
+    lager:warning("Unhandled messages: ~p", [Msg]),
+    {noreply, State}.
+
+%% @private
+-spec handle_info(term(), #state{}) -> {noreply, #state{}}.
+handle_info({tcp, _Socket, Data}, State0) ->
+    handle_message(decode(Data), State0);
+handle_info({tcp_closed, Socket}, State) ->
+    lager:info("Socket closing: ~p", [Socket]),
+    {stop, normal, State};
+handle_info(Msg, State) ->
+    lager:warning("Unhandled messages: ~p", [Msg]),
+    {noreply, State}.
+
+%% @private
+-spec terminate(term(), #state{}) -> term().
+terminate(_Reason, #state{socket=Socket, transport=Transport}) ->
+    Transport:close(Socket),
+    ok.
+
+%% @private
+-spec code_change(term() | {down, term()}, #state{}, term()) ->
+    {ok, #state{}}.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+handle_message({hello, Node}, #state{socket=Socket, transport=Transport}=State) ->
             %% Connect the node with Distributed Erlang, just for now for
             %% control messaging in the test suite execution.
             case net_adm:ping(Node) of
                 pong ->
                     lager:info("Node connected via disterl."),
-                    send_message(Socket, Transport, ok);
+                    send_message(Socket, Transport, ok),
+                    {noreply, State};
                 pang ->
                     lager:info("Node could not be connected."),
-                    send_message(Socket, Transport, {error, pang})
+                    send_message(Socket, Transport, {error, pang}),
+                    {noreply, State}
             end;
-        Response ->
-            lager:info("Invalid response: ~p", [Response]),
-            send_message(Socket, Transport,
-                         {error, {invalid_response, Response}})
-    end,
-
-    %% Close socket.
-    Transport:close(Socket).
-
-%% @private
-receive_message(Socket, Transport) ->
-    receive
-        {tcp_closed, Socket} ->
-            Transport:close(Socket),
-            bo;
-        Message ->
-            decode(Message)
-    end.
+handle_message(Message, #state{socket=Socket, transport=Transport}=State) ->
+    lager:info("Invalid message: ~p", [Message]),
+    send_message(Socket, Transport,
+                 {error, {invalid_response, Message}}),
+    {stop, normal, State}.
 
 %% @private
 send_message(Socket, Transport, Message) ->

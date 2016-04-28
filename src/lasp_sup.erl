94bb2dffa14e5260944382d318e8e60c96866fe7
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-04-28 23:39:20 +0200

    Add additional copyright.

--- src/lasp_sup.erl
+++ src/lasp_sup.erl
@@ -1,6 +1,7 @@
 %% -------------------------------------------------------------------
 %%
 %% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%% Copyright (c) 2016 Christopher Meiklejohn.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file

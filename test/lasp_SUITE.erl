e4dbca58d09392f473873a4b2903ad41f408b89e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-05-17 19:46:25 +0200

    Re-order.

--- test/lasp_SUITE.erl
+++ test/lasp_SUITE.erl
@@ -140,12 +140,12 @@ query_test(_Config) ->
     %% Declare a variable.
     {ok, {I1, _, _, _}} = lasp:declare(ivar),
 
+    %% Change it's value.
+    ?assertMatch({ok, _}, lasp:bind(I1, 2)),
+
     %% Threshold read just to create a synchronization point for the
     %% value to change.
     {ok, _} = lasp:read(I1, {strict, undefined}),
-
-    %% Change it's value.
-    ?assertMatch({ok, _}, lasp:bind(I1, 2)),
 
     %% Query it.
     ?assertMatch({ok, 2}, lasp:query(I1)),

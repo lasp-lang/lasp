1e728e118856be1f0028338146978c5c2688c747
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-06-17 18:30:00 +0200

    More instrumentation.

--- src/lasp_core.erl
+++ src/lasp_core.erl
@@ -846,14 +846,12 @@ reply_to_all([], StillWaiting, _Result) ->
     ok | error.
 receive_delta(Store, {delta_send, {Id, Type, Metadata, Deltas},
                       MetadataFunBind, MetadataFunDeclare}) ->
-    lager:info("Receive delta called."),
     case do(get, [Store, Id]) of
         {ok, _Object} ->
-            lager:info("Object ~p found.", [Id]),
             {ok, _Result} = bind(Id, Deltas, MetadataFunBind, Store);
         {error, not_found} ->
             lager:info("Object ~p not found.", [Id]),
-            {ok, Result} = declare(Id, Type, MetadataFunDeclare, Store),
+            {ok, {Id, Type, _, _} = Result} = declare(Id, Type, MetadataFunDeclare, Store),
             lager:info("Declare called: ~p, result: ~p", [Id, Result]),
             receive_delta(Store, {delta_send, {Id, Type, Metadata, Deltas},
                                   MetadataFunBind, MetadataFunDeclare})

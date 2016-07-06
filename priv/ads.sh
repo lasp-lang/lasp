84bb4ec575b05e7f76ddb6ab81bd5b80644fb979
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-07-06 17:23:31 +0000

    Re-organize to prevent agent reinitialization.

--- priv/ads.sh
+++ priv/ads.sh
@@ -3,16 +3,34 @@
 echo "Removing old code."
 rm -rf priv/evaluation
 
-echo "Installing SSH key."
-( cd priv ; ./install_ssh_key.sh )
+echo "Starting agent."
+eval `ssh-agent -s`
+
+echo "Current SSH_AUTH_SOCK:"
+echo $SSH_AUTH_SOCK
+
+echo "Installing key."
+cp ssh/evaluation /tmp/evaluation
+
+echo "Changing permission on keys."
+chmod 400 /tmp/evaluation
+
+echo "Passphrase is set."
+echo $EVALUATION_PASSPHRASE
+
+echo "Adding key to agent."
+( cd priv ; ./add_key.sh )
+
+echo "Now SSH_AUTH_SOCK:"
+echo $SSH_AUTH_SOCK
+
+echo "After adding key, agent now contains the following"
+ssh-add -l
 
 GIT_SSH=ssh/wrapper
 echo -n "Using the following wrapper: "
 echo -n $GIT_SSH
 echo
-
-echo "Evaluating agent."
-eval `ssh-agent -s`
 
 echo "Authentication socket."
 echo $SSH_AUTH_SOCK

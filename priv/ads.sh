a5e4a523ac1291fd740f55f79d3c7fe809ee4e7c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-07-06 18:43:58 +0200

    Change location of eval.

--- priv/ads.sh
+++ priv/ads.sh
@@ -11,6 +11,9 @@ echo -n "Using the following wrapper: "
 echo -n $GIT_SSH
 echo
 
+echo "Evaluating agent."
+eval `ssh-agent -s`
+
 echo "Cloning evaluation code."
 ( cd priv; GIT_SSH=$GIT_SSH git clone git@github.com:lasp-lang/evaluation.git )
 
@@ -19,9 +22,6 @@ pkill -9 beam.smp
 
 echo "Running evaluation."
 ./rebar3 ct --readable=false --suite=test/lasp_advertisement_counter_SUITE
-
-echo "Evaluating agent."
-eval `ssh-agent -s`
 
 echo "Checking in results."
 ( cd priv/evaluation ; git add . )

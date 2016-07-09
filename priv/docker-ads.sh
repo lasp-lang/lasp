41bad8690ac72aed52370d616d9593d903778d41
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-07-09 14:26:40 +0200

    Don't build image.

--- priv/docker-ads.sh
+++ priv/docker-ads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Building Docker image."
-docker build -f Dockerfiles/lasp-evaluation -t cmeiklejohn/lasp-evaluation .
+# echo "Building Docker image."
+# docker build -f Dockerfiles/lasp-evaluation -t cmeiklejohn/lasp-evaluation .
 
 echo "Launching docker instance."
 docker run --dns=4.2.2.1 -e "EVALUATION_PASSPHRASE=$EVALUATION_PASSPHRASE" -t -i cmeiklejohn/lasp-evaluation

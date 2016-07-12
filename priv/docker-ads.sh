c6bac6c18f5b037f86bf357ae61c6df4cdade5cb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-07-12 15:52:15 +0200

    Build if build is set.
    If the build option is set, then build the docker container first.

--- priv/docker-ads.sh
+++ priv/docker-ads.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# echo "Building Docker image."
-# docker build -f Dockerfiles/lasp-evaluation -t cmeiklejohn/lasp-evaluation .
+if [ ! -z "$BUILD" ]; then
+  echo "Building Docker image."
+  docker build -f Dockerfiles/lasp-evaluation -t cmeiklejohn/lasp-evaluation .
+fi
 
 echo "Launching docker instance."
 docker run --dns=4.2.2.1 -e "EVALUATION_PASSPHRASE=$EVALUATION_PASSPHRASE" -t -i cmeiklejohn/lasp-evaluation

bc552a876f93ac13dc0fb609a473d3291b10eafe
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-07-13 16:50:08 +0200

    Add music festival example.

--- test/lasp_music_festival_SUITE.erl
+++ test/lasp_music_festival_SUITE.erl
@@ -19,7 +19,7 @@
 %% -------------------------------------------------------------------
 %%
 
--module(lasp_advertisement_counter_SUITE).
+-module(lasp_music_festival_SUITE).
 -author("Christopher Meiklejohn <christopher.meiklejohn@gmail.com>").
 
 %% common_test callbacks
@@ -78,23 +78,25 @@ default_test(_Config) ->
     ok.
 
 state_based_with_aae_test(Config) ->
-    lasp_simulation_support:run(state_based_with_aae_test,
+    lasp_simulation_support:run(music_festival_state_based_with_aae_test,
         Config,
         [{mode, state_based},
+         {simulation, music_festival},
          {set, orset},
          {broadcast, false},
-         {evaluation_identifier, state_based_with_aae}]),
+         {evaluation_identifier, music_festival_state_based_with_aae}]),
     ok.
 
 state_based_with_aae_and_tree_test(Config) ->
     case os:getenv("OMIT_HIGH_ULIMIT", "false") of
         "false" ->
-            lasp_simulation_support:run(state_based_with_aae_and_tree_test,
+            lasp_simulation_support:run(music_festival_state_based_with_aae_and_tree_test,
                 Config,
                 [{mode, state_based},
+                 {simulation, music_festival},
                  {set, orset},
                  {broadcast, true},
-                 {evaluation_identifier, state_based_with_aae_and_tree}]),
+                 {evaluation_identifier, music_festival_state_based_with_aae_and_tree}]),
             ok;
         _ ->
             %% Omit.
@@ -102,32 +104,35 @@ state_based_with_aae_and_tree_test(Config) ->
     end.
 
 delta_based_with_aae_test(Config) ->
-    lasp_simulation_support:run(delta_based_with_aae_test,
+    lasp_simulation_support:run(music_festival_delta_based_with_aae_test,
         Config,
         [{mode, delta_based},
+         {simulation, music_festival},
          {set, orset},
          {broadcast, false},
-         {evaluation_identifier, delta_based_with_aae}]),
+         {evaluation_identifier, music_festival_delta_based_with_aae}]),
     ok.
 
 state_based_ps_with_aae_test(Config) ->
-    lasp_simulation_support:run(state_based_ps_with_aae_test,
+    lasp_simulation_support:run(music_festival_state_based_ps_with_aae_test,
         Config,
         [{mode, state_based},
+         {simulation, music_festival},
          {set, awset_ps},
          {broadcast, false},
-         {evaluation_identifier, state_based_ps_with_aae}]),
+         {evaluation_identifier, music_festival_state_based_ps_with_aae}]),
     ok.
 
 state_based_ps_with_aae_and_tree_test(Config) ->
     case os:getenv("OMIT_HIGH_ULIMIT", "false") of
         "false" ->
-            lasp_simulation_support:run(state_based_ps_with_aae_and_tree_test,
+            lasp_simulation_support:run(music_festival_state_based_ps_with_aae_and_tree_test,
                 Config,
                 [{mode, state_based},
+                 {simulation, music_festival},
                  {set, awset_ps},
                  {broadcast, true},
-                 {evaluation_identifier, state_based_ps_with_aae_and_tree}]),
+                 {evaluation_identifier, music_festival_state_based_ps_with_aae_and_tree}]),
             ok;
         _ ->
             %% Omit.
@@ -135,12 +140,13 @@ state_based_ps_with_aae_and_tree_test(Config) ->
     end.
 
 delta_based_ps_with_aae_test(Config) ->
-    lasp_simulation_support:run(delta_based_ps_with_aae_test,
+    lasp_simulation_support:run(music_festival_delta_based_ps_with_aae_test,
         Config,
         [{mode, delta_based},
+         {simulation, music_festival},
          {set, awset_ps},
          {broadcast, false},
-         {evaluation_identifier, delta_based_ps_with_aae}]),
+         {evaluation_identifier, music_festival_delta_based_ps_with_aae}]),
     ok.
 
 %% ===================================================================

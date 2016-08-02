043f20706a4cea43c66ae053e689455cbdb5b230
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-08-02 17:38:45 +0100

    expect eof; exit instead

--- priv/add_key.sh
+++ priv/add_key.sh
@@ -4,4 +4,5 @@ spawn ssh-add "/tmp/evaluation"
 expect "Enter passphrase for /tmp/evaluation:"
 send_user "$env(EVALUATION_PASSPHRASE)\r\n";
 send "$env(EVALUATION_PASSPHRASE)\r\n";
-interact
+expect eof
+exit

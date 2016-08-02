8eaf43f11480b27d13f1049fec3977b449ce7fc1
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-08-02 15:08:23 +0100

    User another image temporarily

--- bin/dcos-deploy.sh
+++ bin/dcos-deploy.sh
@@ -46,7 +46,7 @@ cat <<EOF > lasp-server.json
   "container": {
     "type": "DOCKER",
     "docker": {
-      "image": "cmeiklejohn/lasp-dev",
+      "image": "vitorenesduarte/lasp-dev",
       "network": "HOST",
       "forcePullImage": true,
       "parameters": [
@@ -114,7 +114,7 @@ cat <<EOF > lasp-client.json
   "container": {
     "type": "DOCKER",
     "docker": {
-      "image": "cmeiklejohn/lasp-dev",
+      "image": "vitorenesduarte/lasp-dev",
       "network": "HOST",
       "forcePullImage": true,
       "parameters": [

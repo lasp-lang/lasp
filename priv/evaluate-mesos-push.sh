86c623eeff9ddc72420fb84789565166f0e45b07
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-08-02 18:44:03 +0100

    Retry push + rebase

--- priv/evaluate-mesos-push.sh
+++ priv/evaluate-mesos-push.sh
@@ -39,7 +39,7 @@ git config --global user.name "Lasp Language Evaluation Bot"
 echo "Checking in results."
 ( cd priv/evaluation ; git add . )
 ( cd priv/evaluation ; git commit -m 'Adding evaluation data.' )
-until ( cd priv/evaluation ; GIT_SSH=../$GIT_SSH git push -u origin)
+until ( cd priv/evaluation ; GIT_SSH=../$GIT_SSH git pull --rebase && git push)
 do
   sleep 1
 done

f4042b7ff4544b563f4832d6c4c93c864fdc3780
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-08-02 20:04:51 +0100

    stringify

--- priv/evaluate-mesos-push.sh
+++ priv/evaluate-mesos-push.sh
@@ -46,7 +46,7 @@ R=0
 OK=$(cd priv/evaluation ; GIT_SSH=../$GIT_SSH git push -u origin)
 echo "OK: $OK"
 
-while [ $R -lt $RETRIES -a ${OK} -ne 0 ]
+while [ $R -lt $RETRIES -a "${OK}" -ne "0" ]
 do
   sleep 1
   echo "BEFORE PUSH:"

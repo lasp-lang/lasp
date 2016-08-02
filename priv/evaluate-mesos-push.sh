c77b99f457129b222d964fd715cadb4a8c5888ed
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-08-02 18:42:16 +0100

    Retry push

--- priv/evaluate-mesos-push.sh
+++ priv/evaluate-mesos-push.sh
@@ -39,5 +39,8 @@ git config --global user.name "Lasp Language Evaluation Bot"
 echo "Checking in results."
 ( cd priv/evaluation ; git add . )
 ( cd priv/evaluation ; git commit -m 'Adding evaluation data.' )
-( cd priv/evaluation ; GIT_SSH=../$GIT_SSH git push -u origin)
+until ( cd priv/evaluation ; GIT_SSH=../$GIT_SSH git push -u origin)
+do
+  sleep 1
+done
 

6ca3df0284bc3db356c28b612463d61cb6ae4007
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-08-04 20:17:52 +0100

    Add logging

--- src/lasp_advertisement_counter_client.erl
+++ src/lasp_advertisement_counter_client.erl
@@ -95,7 +95,7 @@ handle_info(log, #state{impressions=Impressions}=State) ->
     %% Get current value of the list of advertisements.
     {ok, Ads} = lasp:query(?ADS_WITH_CONTRACTS),
 
-    lager:info("Impressions: ~p, current ad size: ~p", [Impressions, sets:size(Ads)]),
+    lager:info("Impressions: ~p, current ad size: ~p, node: ~p", [Impressions, sets:size(Ads), node()]),
 
     %% Schedule advertisement counter impression.
     schedule_logging(),

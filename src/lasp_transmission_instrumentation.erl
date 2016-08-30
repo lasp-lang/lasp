1428c58122de3de0b967f5251b6333c27e04c620
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-08-30 17:56:21 +0100

    Change io_lib:format call.

--- src/lasp_transmission_instrumentation.erl
+++ src/lasp_transmission_instrumentation.erl
@@ -188,7 +188,7 @@ dir_path_filename_and_s3_id() ->
       ++ LocalOrDCOS ++ "/"
       ++ atom_to_list(EvalIdentifier),
     EvalTimestamp = lasp_config:get(evaluation_timestamp, 0),
-    Filename = io_lib:format("~s.csv", [node()]),
+    Filename = io_lib:format("~p.csv", [atom_to_list(node())]),
 
     DirPath = log_dir() ++ "/"
         ++ Id ++ "/"

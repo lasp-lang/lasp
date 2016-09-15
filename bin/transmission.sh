81812e9cb94d18719e3ef0d96ff520fd24c70718
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-09-15 17:31:04 +0100

    also run heavy clients with client server

--- bin/transmission.sh
+++ bin/transmission.sh
@@ -33,13 +33,16 @@ MAILBOX_LOGGING=false
 declare -A EVALUATIONS
 
 ## client_server_state_based_with_aae_test
-EVALUATIONS["client_server_state_based_with_aae"]="partisan_client_server_peer_service_manager state_based false false"
+#EVALUATIONS["client_server_state_based_with_aae"]="partisan_client_server_peer_service_manager state_based false false"
 
 ## client_server_delta_based_with_aae_test
 EVALUATIONS["client_server_delta_based_with_aae"]="partisan_client_server_peer_service_manager delta_based false false"
 
+## code_client_server_delta_based_with_aae_test
+EVALUATIONS["code_client_server_delta_based_with_aae"]="partisan_client_server_peer_service_manager delta_based false true"
+
 ## peer_to_peer_state_based_with_aae_test
-EVALUATIONS["peer_to_peer_state_based_with_aae"]="partisan_hyparview_peer_service_manager state_based false false"
+##EVALUATIONS["peer_to_peer_state_based_with_aae"]="partisan_hyparview_peer_service_manager state_based false false"
 
 ## peer_to_peer_state_based_with_aae_and_tree_test
 ##EVALUATIONS["peer_to_peer_state_based_with_aae_and_tree"]="partisan_hyparview_peer_service_manager state_based true false"
@@ -67,8 +70,6 @@ do
 
     if [ "$PEER_SERVICE" == "partisan_client_server_peer_service_manager" ] && [ "$CLIENT_NUMBER" -gt "128" ]; then
       echo "[$(date +%T)] Client-Server topology with $CLIENT_NUMBER clients is not supported"
-    elif [ "$MODE" == "state_based" ] && [ "$PARTITION_PROBABILITY" -gt "0" ]; then
-      echo "[$(date +%T)] Skipping $EVAL_ID with $CLIENT_NUMBER clients; $PARTITION_PROBABILITY % partitions; with configuration $STR"
     else
       PEER_SERVICE=$PEER_SERVICE MODE=$MODE BROADCAST=$BROADCAST SIMULATION=$SIMULATION EVAL_ID=$REAL_EVAL_ID EVAL_TIMESTAMP=$TIMESTAMP CLIENT_NUMBER=$CLIENT_NUMBER HEAVY_CLIENTS=$HEAVY_CLIENTS PARTITION_PROBABILITY=$PARTITION_PROBABILITY AAE_INTERVAL=$AAE_INTERVAL DELTA_INTERVAL=$DELTA_INTERVAL INSTRUMENTATION=$INSTRUMENTATION LOGS=$LOGS AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY EXTENDED_LOGGING=$EXTENDED_LOGGING MAILBOX_LOGGING=$MAILBOX_LOGGING ./dcos-deploy.sh
 

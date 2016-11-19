dac383cc0ee83dffe78ed70f6748332f83646c1c
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-11-19 13:09:06 +0000

    Simplify start-dcos-runner.sh; assumes 'dcos auth login'

--- bin/start-dcos-runner.sh
+++ bin/start-dcos-runner.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+DCOS=$(dcos config show core.dcos_url)
+TOKEN=$(dcos config show core.dcos_acs_token)
+
 ENV_VARS=(
   ID
   LASP_BRANCH
-  DCOS
-  TOKEN
   ELB_HOST
   AWS_ACCESS_KEY_ID
   AWS_SECRET_ACCESS_KEY

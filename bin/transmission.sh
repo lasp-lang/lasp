03365d1a459ff57bf00d23041e9b5038567d8b0b
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-11-29 11:08:24 +0000

    Only run state-based

--- bin/transmission.sh
+++ bin/transmission.sh
@@ -32,9 +32,9 @@ MAILBOX_LOGGING=false
 declare -A EVALUATIONS
 
 # peer service | mode | broadcast (boolean) | heavy_clients (boolean) | reactive_server (boolean)
-#EVALUATIONS["peer_to_peer_state_based_with_aae_test"]="partisan_hyparview_peer_service_manager state_based false false false"
+EVALUATIONS["peer_to_peer_state_based_with_aae_test"]="partisan_hyparview_peer_service_manager state_based false false false"
 #EVALUATIONS["peer_to_peer_state_based_with_aae_and_tree_test"]="partisan_hyparview_peer_service_manager state_based true false false"
-EVALUATIONS["peer_to_peer_delta_based_with_aae_test"]="partisan_hyparview_peer_service_manager delta_based false false false"
+#EVALUATIONS["peer_to_peer_delta_based_with_aae_test"]="partisan_hyparview_peer_service_manager delta_based false false false"
 
 for i in $(seq 1 $EVAL_NUMBER)
 do
@@ -62,7 +62,7 @@ do
       wait_for_completion $TIMESTAMP
       # Marathon may reply that no app is running but the resources may still be unavailable
       # Wait 10 minutes
-      sleep 600
+      # sleep 600
     fi
   done
 

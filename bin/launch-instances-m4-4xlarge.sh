1e35665897c8d6e96b088fe81b74bc3f96e5653b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-12-13 10:42:30 +0100

    Use proper keyname.

--- bin/launch-instances-m4-4xlarge.sh
+++ bin/launch-instances-m4-4xlarge.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+KEY_NAME=dcos
 STACK_NAME=dcos-m4-4xlarge
 
 echo "Launching Oregon."
@@ -7,7 +8,7 @@ aws cloudformation create-stack \
   --stack-name ${STACK_NAME} \
   --template-url https://s3-us-west-2.amazonaws.com/cf-templates-akuyolple9l-us-west-2/2016343Mok-m4.4xlargeqai5dkf1oduzm7ahch6ez5mi \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=KeyName,ParameterValue=${STACK_NAME} \
+  --parameters ParameterKey=KeyName,ParameterValue=${KEY_NAME} \
                ParameterKey=PublicSlaveInstanceCount,ParameterValue=70 \
                ParameterKey=OAuthEnabled,ParameterValue=false \
                ParameterKey=SlaveInstanceCount,ParameterValue=0

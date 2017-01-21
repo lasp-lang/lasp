027d79829b30c31e96aa0c7f6a58b99af9c37b41
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-01-21 07:08:19 +0100

    Restore impressions.

--- src/lasp_sup.erl
+++ src/lasp_sup.erl
@@ -354,8 +354,7 @@ advertisement_counter_child_specs() ->
     %% To have enough impressions for all nodes we need
     %% 180 * client_number impressions
     {ok, ClientNumber} = application:get_env(?APP, client_number),
-    %% TODO: FIX ME.
-    ImpressionNumberDefault = 120 * ClientNumber,
+    ImpressionNumberDefault = 180 * ClientNumber,
     ImpressionNumber = application:get_env(?APP,
                                            max_impressions,
                                            ImpressionNumberDefault),

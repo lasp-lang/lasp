b29346463c61ce76571498a5b81e72a2db8fedcc
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-02-16 18:15:12 +0000

    Don't run 8.

--- test/lasp_client_server_throughput_SUITE.erl
+++ test/lasp_client_server_throughput_SUITE.erl
@@ -39,7 +39,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("kernel/include/inet.hrl").
 
--define(RUNS, [1,2,4,6,8]).
+-define(RUNS, [1,2,4,6]).
 
 %% ===================================================================
 %% common_test callbacks

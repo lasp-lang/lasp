86cf9b131829a8cd4756db33f387cd3c0dde3a9e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-02-23 13:27:08 +0000

    Don't perform blocking sync on the membership object.

--- src/lasp_distribution_backend.erl
+++ src/lasp_distribution_backend.erl
@@ -626,7 +626,15 @@ declare_if_not_found(Result, _Id, _State, _Module, _Function, _Args) ->
 
 %% @private
 blocking_sync(Id) ->
-    ObjectFilterFun = fun(I) -> Id == I end,
+    %% Don't perform blocking sync on the membership object.
+    ObjectFilterFun = fun(I) ->
+                              case ?MEMBERSHIP_ID == I of
+                                  true ->
+                                      false;
+                                  _ ->
+                                      Id == I
+                              end
+                      end,
 
     case lasp_config:get(mode, ?DEFAULT_MODE) of
         state_based ->
